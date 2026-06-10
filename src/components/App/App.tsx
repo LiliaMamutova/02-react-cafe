f4dab984094395417c3649c2851b015fd993ad3d
Lilia Mamutova <lilymalinovskaya@gmail.com>
2026-06-10 22:09:48 +0300

    hw#2 react-cafe

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,9 +2,10 @@ import css from "./App.module.css"
 import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 import VoteOptions from "../VoteOptions/VoteOptions.tsx";
 import {useState} from "react";
-import type {Votes, VoteType} from "../types/votes.ts";
+
 import VoteStats from "../VoteStats/VoteStats.tsx";
 import Notification from "../Notification/Notification.tsx";
+import type {Votes, VoteType} from "../../types/votes.ts";
 
 export default function App() {
     const initialState: Votes = {
